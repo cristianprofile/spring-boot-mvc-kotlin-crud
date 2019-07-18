9c35dcad3333e29f899a2d9f7d17c8af2873784d
cristianprofile <cristianprofile@gmail.com>
2019-07-18 15:39:33 +0200

    Added transactional annotation allowing rollback information after finishing each method

--- src/test/kotlin/com/cromero/api/UserControllerMockMvcTest.kt
+++ src/test/kotlin/com/cromero/api/UserControllerMockMvcTest.kt
@@ -17,9 +17,11 @@ import org.springframework.test.web.servlet.MockMvc
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get
 import org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath
 import org.springframework.test.web.servlet.result.MockMvcResultMatchers.status
+import org.springframework.transaction.annotation.Transactional
 
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 @AutoConfigureMockMvc
+@Transactional
 class UserControllerMockMvcTest (@Autowired  val mockMvc: MockMvc,
                                  @Autowired  val restTemplate: TestRestTemplate,
                                  @Autowired  val userRepository: UserRepository) {
